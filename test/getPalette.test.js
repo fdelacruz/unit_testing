1f0f1626c87a605fc4ce2ba011e1c11f8fb25ccc
Francisco De La Cruz <helloquico@gmail.com>
2015-04-25 09:20:57 -0400

    Build a Test Suite

--- test/getPalette.test.js
+++ test/getPalette.test.js
@@ -2,6 +2,11 @@ var getPalette = require('../lib/getPalette');
 var assert = require('assert');
 
 describe('getPalette', function () {
+	it('should throw an error if the result is not an array', function() {
+		
+		assert.throws(getPalette, /is not an array/);
+	});
+
 	it('should return an array with 3 items', function() {
 
 		var palette = getPalette();	
