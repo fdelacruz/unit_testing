15391fd6501b0e42b05063f98ec60244a94987f2
Francisco De La Cruz <helloquico@gmail.com>
2015-04-25 18:15:37 -0400

    Dependency Injection

--- test/getPalette.test.js
+++ test/getPalette.test.js
@@ -1,13 +1,20 @@
 var getPalette = require('../lib/getPalette');
 var assert = require('assert');
 
-describe('getPalette', function () {
+describe('getPalette', function() {
 	it('should throw an error if the result is not an array', function() {
-		
-		assert.throws(getPalette, /is not an array/);
+
+		function fetch() {
+			return 'not array';	
+		}
+
+		assert.throws(function() {
+			getPalette(fetch);
+		}, /is not an array/);
+
 	});
 
-	it('should return an array with 3 items', function() {
+	it('should return an array with 3 items by default', function() {
 
 		var palette = getPalette();	
 		assert(Array.isArray(palette), 'did not return an array');
