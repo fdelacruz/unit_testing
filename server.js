ce7e46677190287a0d8532ebead2da4e860afbee
Francisco De La Cruz <helloquico@gmail.com>
2015-04-25 00:23:40 -0400

    Write Your First Test

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 var express = require("express");
 var app = express();
+var getPalette = require('./lib/getPalette');
 
 var port = process.env.PORT || 3000;
 
 app.set('view engine', 'jade');
 
 app.get("/", function (req, res) {
-  res.render("index", { palette: ["#cccccc", "#900455", "#0055ff"] });
+	res.render("index", { palette: getPalette() });
 });
 
 app.listen(3000, function() {
