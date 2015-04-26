fab966b9599b7bfbc89324c02ddc4c6a43c3e6b8
Francisco De La Cruz <helloquico@gmail.com>
2015-04-25 22:21:50 -0400

    Asynchronous Testing

--- test/hex2rgb.test.js
+++ test/hex2rgb.test.js
@@ -2,15 +2,23 @@ var hex2rgb = require('../lib/hex2rgb');
 var assert = require('assert');
 
 describe('hex2rgb', function() {
-	it('should throw an error if the value is not a hex number', function() {
-		assert.throws(function() {
-			hex2rgb('blue');
-		}, /Invalid hexadecimal number/);
+
+	it('should return an error if the value is not a hex number', function(done) {
+
+		hex2rgb('blue', function(error, result) {
+			assert(error);
+			done();
+		});
+
 	});
 
-	it('should return a correctly converted rgb value', function() {
+	it('should return a correctly converted rgb value', function(done) {
 
-		var rgb = hex2rgb("#fff");
-		assert.deepEqual(rgb, [255, 255, 255]);
+		hex2rgb("#fff", function(error, result) {
+			assert.strictEqual(error, null);
+			assert.deepEqual(result, [255, 255, 255]);
+
+			done();
+		});
 	});
 });
